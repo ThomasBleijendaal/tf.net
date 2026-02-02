e256723b4da7c2b66bfa15fd996b572996e13432
Thomas Bleijendaal <t.bleijendaal@wearetriple.com>
2026-02-02 09:36:54 +0100

    Message pack wip

--- src/TfNet/Serialization/DefaultDynamicValueSerializer.cs
+++ src/TfNet/Serialization/DefaultDynamicValueSerializer.cs
@@ -1,10 +1,13 @@
 ﻿using System.Text.Json;
-using MessagePack;
+using Nerdbank.MessagePack;
+using PolyType.ReflectionProvider;
 
 namespace TfNet.Serialization;
 
 public class DefaultDynamicValueSerializer : IDynamicValueSerializer
 {
+    private readonly MessagePackSerializer _serializer = new();
+
     public T DeserializeJson<T>(ReadOnlyMemory<byte> value)
     {
         return JsonSerializer.Deserialize<T>(value.Span, new JsonSerializerOptions { PropertyNameCaseInsensitive = true })
@@ -17,18 +20,26 @@ public class DefaultDynamicValueSerializer : IDynamicValueSerializer
              ?? throw new InvalidOperationException("Invalid Json provided");
     }
 
-    public T DeserializeMsgPack<T>(ReadOnlyMemory<byte> value)
+    public T? DeserializeMsgPack<T>(ReadOnlyMemory<byte> value)
     {
-        return MessagePackSerializer.Deserialize<T>(value);
+        var shape = ReflectionTypeShapeProvider.Default.GetTypeShape<T>();
+        var reader = new MessagePackReader(value);
+
+        return _serializer.Deserialize(ref reader, shape, default);
     }
 
     public object? DeserializeMsgPack(Type type, ReadOnlyMemory<byte> value)
     {
-        return MessagePackSerializer.Deserialize(type, value);
+        var shape = ReflectionTypeShapeProvider.Default.GetTypeShape(type);
+        var reader = new MessagePackReader(value);
+
+        return _serializer.DeserializeObject(ref reader, shape, default);
     }
 
     byte[] IDynamicValueSerializer.SerializeMsgPack<T>(T value)
     {
-        return MessagePackSerializer.Serialize(value);
+        var shape = ReflectionTypeShapeProvider.Default.GetTypeShape<T>();
+
+        return _serializer.Serialize(value, shape);
     }
 }
